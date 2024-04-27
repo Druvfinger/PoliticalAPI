965e6ea2a805b44f0d02fb21b1506fbc9986ec59
Richard Davidsson <richard.davidsson@yh.nackademin.se>
2024-04-27 13:16:35 +0200

    finally got things to load variables correctly

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
     implementation("org.jsoup:jsoup:1.17.2")
-    implementation("com.mysql:mysql-connector-j:8.2.0")
+    implementation("com.mysql:mysql-connector-j:8.3.0")
     implementation("io.github.cdimascio:dotenv-java:3.0.0")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
--- src/main/java/com/example/politico/PoliticoApplication.java
+++ src/main/java/com/example/politico/PoliticoApplication.java
@@ -1,14 +1,24 @@
 package com.example.politico;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+import java.util.Properties;
 
 @SpringBootApplication
 
 public class PoliticoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PoliticoApplication.class, args);
+
+        Dotenv dotenv = Dotenv.configure().load();
+        SpringApplication app = new SpringApplication(PoliticoApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("spring.datasource.url", dotenv.get("DB_URL")));
+        app.setDefaultProperties(Collections.singletonMap("spring.datasource.username", dotenv.get("DB_USER")));
+        app.setDefaultProperties(Collections.singletonMap("spring.datasource.password", dotenv.get("DB_PASS")));
+        app.run(args);
 
     }
 
